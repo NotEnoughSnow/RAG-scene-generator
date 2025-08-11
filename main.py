0e588998714c6fbfa8b36b6ba362775f4bbede27
Amin Mraidi <amine.mraidi@outlook.com>
2025-08-11 10:12:35 +0200

    added general plan with main.py

---
+++ main.py
@@ -0,0 +1,15 @@
+def main():
+
+    query = "good evening"
+
+    # preprocess query
+
+    # retrieve context
+
+    # generate image
+
+    # display description + image
+
+
+if __name__ == "__main__":
+    main()
